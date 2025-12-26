607ecb2b9a5484669c72dc4224ff0d33016e6e2c
Adrian Polo <adrian.polo.romani@gmail.com>
2025-12-26 15:28:43 -0500

    Added prereq for entity framework in entities

--- src/Tracker.Domain/Entities/CustomerType.cs
+++ src/Tracker.Domain/Entities/CustomerType.cs
@@ -2,4 +2,6 @@ public class CustomerType
 {
     public int CustomerTypeId { get; private set; }
     public string Description { get; private set; } = null!;
+
+    internal CustomerType() {}
 }
--- src/Tracker.Domain/Entities/ShipmentStatus.cs
+++ src/Tracker.Domain/Entities/ShipmentStatus.cs
@@ -2,4 +2,6 @@ public class ShipmentStatus
 {
     public int ShipmentStatusId { get; private set; }
     public string Description { get; private set; } = null!;
+
+    internal ShipmentStatus() {}
 }
